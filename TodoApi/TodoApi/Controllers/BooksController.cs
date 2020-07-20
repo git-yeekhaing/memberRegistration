72b670eec460d9b40704eef3b70329c6e04135be
git-yeekhaing <yee.khaing.git@gmail.com>
2020-07-20 18:30:04 +0900

    asp dot net core api and EF core

---
+++ TodoApi/TodoApi/Controllers/BooksController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Data;
+using TodoApi.Entities;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookContext context;
+
+        public BooksController(BookContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> List()
+        {
+            var books = context.Books;
+            return books;
+        }
+    }
+}
---
+++ TodoApi/TodoApi/Data/BookContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Entities;
+
+namespace TodoApi.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
